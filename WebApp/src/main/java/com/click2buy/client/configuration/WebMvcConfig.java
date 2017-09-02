889243eee4b8d9d3c4c4dc3787d1171bc312b188
yurii.kachmar <yurii.kachmar@eleks.com>
2017-09-02 21:53:49 +0300

    TA0006: Add Password Hasher

---
+++ WebApp/src/main/java/com/click2buy/client/configuration/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.click2buy.client.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+}
