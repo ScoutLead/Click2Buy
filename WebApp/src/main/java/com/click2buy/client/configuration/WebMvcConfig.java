967f97add8587d4da21c2a3f946cf089730f1c1f
yurii.kachmar <yurii.kachmar@eleks.com>
2017-09-03 15:52:24 +0300

    Add annotation to WebMvcConfig

--- WebApp/src/main/java/com/click2buy/client/configuration/WebMvcConfig.java
+++ WebApp/src/main/java/com/click2buy/client/configuration/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.click2buy.client.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter{
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
