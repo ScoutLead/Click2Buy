2a1880108e1d59fe86b1a454d49984d66e706eb4
ScoutLead <kachmarani@gmail.com>
2018-05-15 16:37:59 +0300

    Revert "TA0009 Add newest goods on home page"

--- WebApp/src/main/java/com/click2buy/client/security/dto/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.click2buy.client.security.dto;
-
-public class LoginResponse {
-  public final String token;
-
-  public LoginResponse(String token) {
-    this.token = token;
-  }
-}
--- WebApp/src/main/java/com/click2buy/client/service/ProductsService.java
+++
@@ -1,9 +0,0 @@
-package com.click2buy.client.service;
-
-import com.click2buy.client.model.Product;
-import java.util.List;
-import java.util.Map;
-
-public interface ProductsService {
-  Map<String, List<Product>> getCategoriesNameAndNewestProducts();
-}
